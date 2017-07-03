741655608094d600617a0f9e46686cea7018eba6
viktorijagardauskiene <viktorija.gardauskiene@gmail.com>
2017-07-03 19:13:25 +0300

    sukusrtas templeitas kuriame itrauktas footer failas

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Puslapis</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+
+<body>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-12"><h1>Header</h1></div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">Content</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12"><?php include "inc/footer.php" ?></div> <!-- iterpiame atskira footer file kuriame bus reikiamas kodas -->
+	</div>
+
+</div>
+
+
+</body>
+</html>
