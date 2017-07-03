beebdf2becb2891c6942ec873908fa60616eb119
viktorijagardauskiene <viktorija.gardauskiene@gmail.com>
2017-07-03 20:30:42 +0300

    prideta sesija
    kuriai priskirtas tas sablonas kuri pasirinksim, kad perkrovus narsykle jis liks tas pats o ne default, ir isliks priskirtas kol isjungsim narsykle

--- index.php
+++ index.php
@@ -1,14 +1,21 @@
 <?php 
 
+session_start();
+
 $title = "Puslapis";
 $content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 $templates = ["lightyellow", "green", "orange", "pink"];
 
+
 if (isset($_GET['t'])) { // uzkrovus puslapi rodo klaida nes nera jokios "t" reiksme, todel patikrinam ar uzkrovus puslapi buvo pasirinkta kokia reiksme, jei ne tada naudoja default pirma masyvo reiksme
 $template_name = $_GET['t']; // template_selector/<select> name yra "t"
+$_SESSION['session_template'] = $_GET['t']; // sesijai priduodam reiksme ta kuria pirmiausia pasirinksim spalva, ir ji isliks tol kol isjungsim narsykle
+} else if (isset($_SESSION['session_template'])) {
+	$template_name = $_SESSION['session_template']; 
 } else {
 	$template_name = $templates[0];
 }
+
 
 include 'sablonai/' . $template_name . '.php';
  
